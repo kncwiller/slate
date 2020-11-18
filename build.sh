ef5397b19ec22e7275968fe437ba042948404724
tex_willer_ <kncwiller@gmail.com>
2020-11-18 11:48:59 +0100

    build file

---
+++ build.sh
@@ -0,0 +1,3 @@
+cp -r source/* /home/mpata/config/slate/source
+docker build -t slatedocs/slate .
+docker run -d --rm --name slate -p 4567:4567 -v /home/mpata/config/slate/source:/srv/slate/source slatedocs/slate serve
